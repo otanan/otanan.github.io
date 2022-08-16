d7987ef7ba89ab638d3e61d2210eeebd5052847b
Jonathan Delgado <jonathandelgado0327@gmail.com>
2022-08-15 23:55:17 -0400

    improved reveal-button functionality and JS logic

--- js/tags.js
+++ js/tags.js
@@ -4,7 +4,37 @@
     * @file   tags.js.
     * @author Jonathan Delgado.
  */
+ /*======================= Fields =======================*/
+const ANIM_STYLE = 'slow';
 /*======================= Helper =======================*/
+/**
+ * Clears the hash from the URL.
+ */
+function clearHash() {
+    history.replaceState(null, null, ' ');
+}
+
+
+/**
+    * Highlights contact me information.
+    */
+function contactHighlight() {
+    // Do nothing if this is not a contact hash
+    if (location.hash !== '#contact') { return; }
+
+    // Go to hash
+    $(location.hash).slideDown(ANIM_STYLE);
+    // Highlight the contact info
+    $('mark#contact-mark').animate({
+        backgroundColor: '#6CA3D8',
+    }, 1500)
+    .delay(500)
+    .animate({
+        backgroundColor: 'transparent',
+    }, 1500);
+}
+
+
 /**
     * Gets camelcase version of string, i.e. Complex Analysis.
     * complexAnalysis.
@@ -71,20 +101,15 @@ customElements.define('github-link', class ContactMe extends HTMLElement {
 customElements.define('grid-image', class GridImage extends HTMLElement {
     constructor() {
         super();
-        // Div and span container
-        var container = document.createElement('div');
-        container.className = 'col-4';
-        this.parentElement.appendChild(container);
-        var span = document.createElement('span');
-        span.className = 'image fit';
-        container.appendChild(span);
+
+        var container = this.makeContainer();
 
         var href = this.getAttribute('img-link');
         if (href != null) {
             var link = document.createElement('a');
             link.href = href
             link.target = '_blank';
-            span.appendChild(link);
+            container.appendChild(link);
         }
 
         var img = document.createElement('img');
@@ -92,7 +117,7 @@ customElements.define('grid-image', class GridImage extends HTMLElement {
         if (href != null) {
             link.appendChild(img)
         } else {
-            span.appendChild(img)
+            container.appendChild(img)
         }
 
         var captionText = this.getAttribute('img-caption');
@@ -101,100 +126,138 @@ customElements.define('grid-image', class GridImage extends HTMLElement {
             var caption = document.createElement('span');
             caption.textContent = captionText;
             caption.className = 'col-12 caption';
-            span.appendChild(caption)
+            container.appendChild(caption)
         }
 
         // Delete own tag since template CSS depends on immediate child
         this.parentElement.removeChild(this);
     }
+    /**
+        * Make the container with special classes required for proper styling 
+        * holding the image.
+        */
+    makeContainer() {
+        // Div and span container
+        var container = document.createElement('div');
+        // Make use of template classes
+        container.className = 'col-4';
+        this.parentElement.appendChild(container);
+        var span = document.createElement('span');
+        span.className = 'image fit';
+        container.appendChild(span);
+
+        return span;
+    }
 });
 
 
-
 /**
-    * Initializes reveal buttons.
+    * Defines the reveal button with selected state.
     */
-function initRevealButtons() {
-    var buttons = $('reveal-button');
+class RevealButton extends HTMLElement {
+    // PDF container tied to all buttons
+    static pdfContainer = null;
+    static active = null;
+    // Dictionary of buttons by their pdf anchors.
+    static buttons = {};
 
-    for (var button of buttons) {
+    constructor() {
+        super();
+        /*-- Tag information --*/
         // Example label: Curriculum Vitae
-        label = button.innerHTML.trim()
-        // Example name: cv
-        name = $(button).attr('name');
-        if (name == 'undefined') {
-            // Get default name if attribute isn't provided
-            name = label
+        var label = this.getAttribute('button-label');
+        // Button name, for anchoring and changing hash
+        // Example name: cv, realAnalysis
+        this.pdfAnchor = this.getAttribute('button-name');
+        this.pdfAnchor = (this.pdfAnchor == undefined) ? camelize(label) : camelize(this.pdfAnchor);
+        // Link to the pdf to reveal
+        var pdfLink = this.getAttribute('pdf-link');
+
+        /*-- Button creation --*/
+        this.link = document.createElement('a');
+        this.link.innerText = label;
+        this.link.className = 'unselectable large button';
+        this.appendChild(this.link);
+
+        this.initPDFContainer(pdfLink);
+
+        // Bind click event
+        this.addEventListener('click', this.toggle);
+        // Save button
+        RevealButton.buttons[this.pdfAnchor] = this;
+    }
+    /**
+        * Creates pdf view in pdf container.
+        */
+    initPDFContainer(pdfLink) {
+        if (RevealButton.pdfContainer == null) {
+            // Find the first container
+            RevealButton.pdfContainer = $('#pdfContainer')[0];
         }
-        // Link to the pdf
-        pdfLink = $(button).attr('pdf-link')
-        // Example button container anchor: realAnalysis
-        containerAnchor = camelize(name)
-        // Example button id: realAnalysisButton
-        id = containerAnchor + 'Button'
-        // The button functionality
-        onclickCommand = `toggleRevealButtonChosen("#${containerAnchor}");`
-        // Make the button
-        button.innerHTML = `
-        <a id='${id}' onclick='${onclickCommand}'
-        class='button large'>
-            ${label}
-        </a>
-        `
-        /*-- PDF container --*/
-        initPDFContainer(containerAnchor, pdfLink)
+        this.pdfdiv = document.createElement('div');
+        // Start hidden
+        this.pdfdiv.style.display = 'none';
+        RevealButton.pdfContainer.appendChild(this.pdfdiv);
+
+        // Linking anchor, offset in CSS
+        var anchor = document.createElement('a');
+        anchor.className = 'anchor';
+        anchor.id = this.pdfAnchor;
+        this.pdfdiv.appendChild(anchor);
+
+        var pdfFrame = document.createElement('iframe');
+        pdfFrame.src = pdfLink;
+        pdfFrame.width = '100%';
+        pdfFrame.height = '800';
+        this.pdfdiv.appendChild(pdfFrame);
+    }
+    /**
+        * Handles toggle logic by activating and deactivating corresponding 
+        * buttons.
+        * @param {arg1 type} arg1 name - arg1 description.
+        */
+    toggle() {
+        if (RevealButton.active == null) {
+            // No button is active, this one will be
+            this.select();
+        } else if (RevealButton.active == this) { 
+            // This is active, none will be
+            this.deselect();
+        } else {
+            // Replace old button with new
+            RevealButton.active.deselect();
+            this.select();
+        }
+    }
+
+
+    select() {
+        RevealButton.active = this;
+        location.hash = '#' + this.pdfAnchor;
+        $(this.link).addClass('chosen');
+        // Show pdf
+        $(this.pdfdiv).slideDown(ANIM_STYLE);
+    }
+
+
+    deselect() {
+        RevealButton.active = null;
+        clearHash();
+        $(this.link).removeClass('chosen');
+        // Hide pdf
+        $(this.pdfdiv).slideUp(ANIM_STYLE);
     }
 }
-
-
-/**
-    * Initializes the PDF container for the id associated to
-    * given reveal button.
-    * @param {str} containerAnchor - the anchor to use for linking from another 
-    * button.
-    * @param {str} pdfLink - the link to the pdf to show.
-    */
-function initPDFContainer(containerAnchor, pdfLink) {
-    container = $('#pdfContainer')[0];
-    container.innerHTML += `
-        <div id='${containerAnchor}' style='display:none;'>
-            <a name='${containerAnchor}'></a>
-            <iframe src='${pdfLink}' width='100%' height='800'></iframe>
-        </div>
-    `
-}
-
-
-
-/*======================= Functionality =======================*/
-
-
-/**
-    * Highlights contact me information.
-    */
-function contactHighlight() {
-    // Do nothing if this is not a contact hash
-    if (location.hash !== '#contact') { return; }
-
-    // Go to hash
-    $(location.hash).slideDown(animStyle);
-    // Highlight the contact info
-    $('mark#contact-mark').animate({
-        backgroundColor: '#6CA3D8',
-    }, 1500)
-    .delay(500)
-    .animate({
-        backgroundColor: 'transparent',
-    }, 1500);
-}
+customElements.define('reveal-button', RevealButton);
 
 
 /*======================= Entry =======================*/
-$(function() {
-    $(window).bind(
-        'hashchange',
-        contactHighlight()
-    );
-});
-
-initRevealButtons();
+if (location.hash === '#contact') {
+    contactHighlight();
+}
+// Check hash for a button to start as active
+if (location.hash != '') {
+    var button = RevealButton.buttons[location.hash.replace('#', '')];
+    // The button exists, select it
+    if (button != undefined) { button.select(); }
+}
